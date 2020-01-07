79047a5de00ca5f877f698b3fce3695df0ccc845
sanjoyk <sanjoyksardar12@gmail.com>
2020-01-08 01:33:19 +0530

    elastic scroll demo

--- index.js
+++ index.js
@@ -8,5 +8,27 @@ module.exports = postcss.plugin('postcss-fallback-font-family', (opts = { }) => 
 
     // Transform CSS AST here
 
+    //looping through each selector block
+    root.walkRules(function(rule){
+
+      //looping through each property
+      // rule.walkDecls(function(decl){
+      //   //we work with each "decl" object here
+      //   //decl.prop.indexOf('overflow) ===0
+      // })
+      rule.walkDecls(/^overflow-?/, function(decl){
+        var hasTouch = rule.some(function(i){
+          return i.prop === "-webkit-overflow-scrolling";
+        });
+        if(!hasTouch){
+          if(decl.value === "scroll"){
+            rule.append({
+              prop: "-webkit-overflow-scrolling",
+              value: "touch"
+            });
+          }
+        }
+      });
+    })
   }
 })
